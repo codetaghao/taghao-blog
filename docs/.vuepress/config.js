be434bfc9fed6be66649d0921a6204c52583d90d
taghaoCode <taghao123@163.com>
2020-09-27 15:09:23 +0800

    添加导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,18 @@ module.exports = {
   title: 'Taghao',
   description: '前端学习个人博客',
   themeConfig: {
+    sidebar: 'auto',
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'External', link: 'https://google.com' },
+      { text: '单词', link: '/' },
+      { text: '移动web', link: '/guide/' },
+      { text: 'JS', link: 'https://google.com' },
+      { text: 'jQuery', link: 'https://google.com' },
+      { text: 'ajax', link: 'https://google.com' },
+      { text: 'nodeJs', link: 'https://google.com' },
+      { text: 'Vue', link: 'https://google.com' },
+      { text: '微信小程序', link: 'https://google.com' },
+      { text: 'react', link: 'https://google.com' },
     ]
   },
-  themeConfig: {
-    sidebar: 'auto'
-  }
+  
 }
